13e25412531453bf3263c7240cc5af0b5b43f16d
AlamsyahRiyo <riyoalamsyah@student.uns.ac.id>
2023-05-10 23:50:13 +0700

    tugasweek6

---
+++ latihan_Modul06.py
@@ -0,0 +1,19 @@
+def fungsi():
+    print("halo praktikkan")
+
+fungsi()
+
+
+def fungsi():
+    print("hello world")
+
+fungsi()
+
+print("diluar fungsi")
+
+
+#Fungsi dengan argumen
+
+def add_numbers(num1,num2):
+    sum = num1+num2
+    print("Sum = ",sum)
